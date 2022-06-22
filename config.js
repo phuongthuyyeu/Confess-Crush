433612358474bdcf6422c8f02b2b20fdb9776b0b
phuongthuyyeu <108014928+phuongthuyyeu@users.noreply.github.com>
2022-06-22 23:08:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    titleWeb: "my bae",
+    introTitle: 'Phuong Thuy',
+    introDesc: `heyyy,anh tên là Tuấn, nhưng từ giờ em có thể gọi là babyyyyyeee',
+    btnIntro: '^^Pthuy ấn vào đây nàooo^^',
+    title: 'nè , em có thích tôi không...? <3',
+    desc: 'em thoát ra là em đồng ý đó nkaaaa <33 ',
+    btnYes: 'Thích anh lắm ạ  <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    question: 'Em có biết vì sao anh lại xuất hiện ở đây không,à với lại anh có đẹp trai không? ? <3',
+    btnReply: ' Gửi cho anh nèeee <3',
+    reply: 'có ạ, anh rất đẹp trai và đến đây là vì em <3 <3 <3',
+    mess: 'đúng ruifiii , là vì em đóooo 😘😘',
+    messDesc: 'thứ 2 là ngày đầu tuần,bé hứa cô gắng chăm ngoan,t3 t4 t5 ngày nào cũng luôn cố gắng ,t6 rồi đến thứ 7 tuần này em có rảnh không?.',
+    btnAccept: 'dạ em rảnh,qua đón em đi coi phim nhé <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100006131343650' 
 }
